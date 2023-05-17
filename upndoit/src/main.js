09f5b7634a933ed07fbb1cefca0d4e998491ee65
DQTree <dc.queijo01@gmail.com>
2023-05-17 12:13:25 +0100

    Added palette link

--- upndoit/src/main.js
+++ upndoit/src/main.js
@@ -1,3 +1,5 @@
+//  Palette used from:
+//  https://colorhunt.co/palette/fbfacddebaceba94d17f669d
 import { createApp } from 'vue'
 import { plugin, defaultConfig } from '@formkit/vue'
 //import App from './App.vue'
